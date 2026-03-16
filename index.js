cfb94eb2a864b5117fb4ed40da5c8c1e1494dec9
village-coders <villagecoders7@gmail.com>
2026-03-16 17:33:12 +0100

    sdfsdsdfsdf

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 // Enable CORS
 app.use(cors({
-    origin: [process.env.CORS_ORIGIN || "http://localhost:5173"],
+    origin: [process.env.CORS_ORIGIN, "http://localhost:5173"],
     credentials: true
 }));
 
