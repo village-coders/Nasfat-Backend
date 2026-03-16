3ae9aa5d5ba44717cc4f525f14a1067b4e805c01
village-coders <villagecoders7@gmail.com>
2026-03-16 23:22:14 +0100

    sdfdfdfdf

--- src/models/Admin.js
+++ src/models/Admin.js
@@ -26,9 +26,9 @@ const adminSchema = new mongoose.Schema({
 });
 
 // Encrypt password using bcrypt
-adminSchema.pre('save', async function (next) {
+adminSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
